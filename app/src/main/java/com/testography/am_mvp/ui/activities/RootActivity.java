6a018b91111f494a89574a1a790bc9dd2d1ba1eb
Leo Burdianov <leo.burdianov@gmail.com>
2016-11-14 00:52:25 +0400

    Add cart image inside Toolbar and avatar image (not yet movable)

--- app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
+++ app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
@@ -14,14 +14,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.testography.am_mvp.BuildConfig;
 import com.testography.am_mvp.R;
@@ -98,6 +102,18 @@ public class RootActivity extends AppCompatActivity implements IRootView,
                     .replace(R.id.fragment_container, new CatalogFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem item = menu.findItem(R.id.badge);
+        MenuItemCompat.setActionView(item, R.layout.badge_layout);
+        RelativeLayout notifCount = (RelativeLayout) MenuItemCompat.getActionView(item);
+
+        TextView tv = (TextView) notifCount.findViewById(R.id.items_in_cart);
+        tv.setText("12");
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
--- app/src/main/java/com/testography/am_mvp/ui/fragments/AddressFragment.java
+++ app/src/main/java/com/testography/am_mvp/ui/fragments/AddressFragment.java
@@ -19,7 +19,6 @@ public class AddressFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
